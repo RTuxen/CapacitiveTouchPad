9ddf42d61610f67f71c32610a270444c0be7aaed
RTuxen <39836677+RTuxen@users.noreply.github.com>
2019-06-08 14:23:42 +0200

    Added ROM counting sensors

--- Arduino/keypad.ino
+++ Arduino/keypad.ino
@@ -16,38 +16,43 @@ long frekvens = 2;          // Wanted frequency - Falls of at 5kHz when no print
 long T = 1000000/frekvens/2;// Period of the frequency
 long tid = 0;               // Real time value
 long tid_last = 0;          // Time
-int cnt = -1;               // Row-counter
-int row = 0;                // yes
-int row_last = 0;           // yes-last
+
+// Values for CVD
+int row = 0;                // Current row number
+int column = 0;             // Current column number
+int columnLast = 0;
+bool columnCountingFinished = false;
+
+// Values for ROM
+int sensor_ROM = 0;
+int sensor_ROM_Last = 0;
+int touchMatrix_ROM[7] = {0,0,0,0,0,0,0};
+int benchmark_ROM[7] = {0,0,0,0,0,0,0};
+int row_ROM[3] = {0,0,0};
+int column_ROM[4] = {0,0,0,0};
 
 // Values used in pin counting
-int sampleTime = 100;         // Samples every 100 ms
-unsigned long timeMark = 0;   // 
-unsigned long currentTime = 0;//
 int k,i;
-int touchMatrix[4][3] = {
-  {0, 0, 0},
-  {0, 0, 0},
-  {0, 0, 0},
-  {0, 0, 0}
+int touchMatrix[3][4] = {
+  {0, 0, 0, 0},
+  {0, 0, 0, 0},
+  {0, 0, 0, 0}
 };
-int benchmark[4][3] = {
-  {0, 0, 0},
-  {0, 0, 0},
-  {0, 0, 0},
-  {0, 0, 0}
+int benchmark[3][4] = {
+  {0, 0, 0, 0},
+  {0, 0, 0, 0},
+  {0, 0, 0, 0}
 };
-int touch[4][3] = {
-  {0, 0, 0},
-  {0, 0, 0},
-  {0, 0, 0},
-  {0, 0, 0}
+int touch[3][4] = {
+  {0, 0, 0, 0},
+  {0, 0, 0, 0},
+  {0, 0, 0, 0}
 };
     
 void setup() {
     Serial.begin(9600);
 
-    // Sets pin 4 and 7 as outputs - rest as inputs
+    // Sets pin 4 and 6 as outputs - rest as inputs
     DDRD = B10010000;
 
     // Callibrates selector
@@ -58,20 +63,47 @@ void setup() {
 }
 
 void loop() {
-  // Finds current row
-  row_last = row;
-  row = row_counter();    
+  ///////////////////////
+  //////////CVD//////////
+  ///////////////////////
+  
+  row_column_counter_CVD(); // Counts rows and columns
+  if((column != columnLast)){
+    Serial.print("Row : ");
+    Serial.print(row);
+    Serial.print(" column: ");
+    Serial.println(column);
+    columnLast = column;
+  }
+  touchCounter_CVD();     // Counts tics on input pin
+  
 
-  touchCounter();     // Counts tics on input pins
+  ///////////////////////
+  //////////ROM//////////
+  ///////////////////////
+  /*
+  row_column_counter_ROM(); // Counts sensors
+  touchCounter_ROM();       // Counts tics on input pin
+  
+  if(sensor_ROM != sensor_ROM_Last){
+    Serial.print("Sensor : ");
+    Serial.println(sensor_ROM);
+    sensor_ROM_Last = sensor_ROM;
+  }
+  */
   
   if (countingCycleFinished){
+    Serial.println("COUNTING CYCLE FINISHED");
     countingCycleFinished = false;
     key = determineWhatButtonWasPressed();
 
     if (( key >= 0) && (key <= 9) && (lastKey != key)){    // If input is between 0-9
+      if(index == 4){
+        index--;
+      }
       touchCode[index] = key;
       index++;
-
+    } else if((key == 10) && (lastKey !=key)){
       if (index == lenCode){    // If password attempt is finished
         if (memcmp(touchCode, password, sizeof(touchCode)) == 0){
           Serial.println("CORRECT CODE");
@@ -79,31 +111,25 @@ void loop() {
         } else{
           Serial.println("WRONG CODE");
         }
+      } else{
+        Serial.println("WRONG CODE");
+      }
+      index = 0;
+      memset(touchCode,-1, lenCode*4);
+    } else if((key == 11) && (lastKey !=key)){
+      index--;
+      if(index < 0){
         index = 0;
       }
+      touchCode[index] = -1;
     }
     lastKey = key;
   }
 }
 
-// Enables user to change password
-void newCode(int buf[], int size) {
-  int key;
-  int j=0;
-
-  for (int g = 0; g < size; g++) {
-    buf[g] = -1;
-  }
-
-  while (j < size){
-    printf("\nNEW PASSWORD:  ");
-    scanf(" %d", &key);
-    if ((key >= 0) && (key <= 9)){    // If input is between 0-9
-      buf[j] = key;
-      j++;
-    }
-  }
-}
+///////////////////////////////////////////////////////////
+//////////////////INITIALIZATION///////////////////////////
+///////////////////////////////////////////////////////////
 
 // Initializes counter pins
 void set_selector() {
@@ -131,13 +157,17 @@ void set_selector() {
   PORTD = PORTD & B01101111;
 }
 
-// Counts rows
-int row_counter() {
+///////////////////////////////////////////////////////////
+//////////////////INTEGRATOR///////////////////////////////
+///////////////////////////////////////////////////////////
+
+// Counts columns
+void row_column_counter_CVD() {
   // Gets time
-  tid = micros() - tid_last;
+  tid = micros();
   
   // If time to switch
-  if (tid > T) {
+  if ((tid - tid_last > T)) {
     tid_last = micros();
       
     if (!flag){ // LOW
@@ -148,34 +178,32 @@ int row_counter() {
       // cnt - return value of current row
       PORTD = PORTD & B01111111;
       flag = false;
-      cnt++;
-      if (cnt == 4) cnt = 0;    
-      flag = false;
-     }
+      column++;
+      if(column == 4){
+        columnCountingFinished = true; 
+        column = 0;
+      } else if ((row == 2) && (column == 3)){
+        countingCycleFinished = true;
+      }
+    }
   }
-  return cnt;
+  // Switches sensor
+  if(columnCountingFinished){
+    row++;
+    if(row == 3){
+      row = 0;
+    }
+    columnCountingFinished = false;
+  }
 }
 
-void touchCounter(){
-  // Displays message if new row
-  if (row != row_last) {
-    //Serial.print("Current row is: ");
-    //Serial.println(row);
-  }
-  
-  // Reads input on pin 4, 2 and 3
-  // Reads input on d2, d3, d4 for cycles
-  // 4 = d4, 1 = d3, 0 = d2
-  // PIND = PIND & B00010011;
-  if (PIND & B00010000) touchMatrix[row][0]++;
-  if (PIND & B00000010) touchMatrix[row][1]++;
-  if (PIND & B00000001) touchMatrix[row][2]++;
+void touchCounter_CVD(){  
+  // Reads input on pin 2 (PD2)
+  if (PIND & B00000010) touchMatrix[row][column]++;
 
-  currentTime = millis();
-  if(currentTime-timeMark >= sampleTime){
-    timeMark = currentTime;
-    for (i = 0; i <= 3; i++){
-      for (k = 0; k <= 2; k++){
+  if(countingCycleFinished){
+    for (i = 0; i <= 2; i++){
+      for (k = 0; k <= 3; k++){
         if(touchMatrix[i][k] >= benchmark[i][k]){
           touch[i][k] = 1;
         } else{
@@ -184,9 +212,80 @@ void touchCounter(){
       }
     }
     memset(touchMatrix, 0, sizeof(touchMatrix[0][0])*4*3); // Clears touch matrix
-    countingCycleFinished = true;
   }
 }
+
+///////////////////////////////////////////////////////////
+////////////RELAXATION OSCILLATOR//////////////////////////
+///////////////////////////////////////////////////////////
+
+
+// Sensor counter for ROM
+void row_column_counter_ROM(){
+  // Gets time
+  tid = micros();
+  
+  // If time to switch
+  if ((tid - tid_last > T)) {
+    tid_last = micros();
+      
+    if (!flag){ // LOW
+      flag = true;
+      PORTD = PORTD | B10000000;
+      }
+    else if (flag) { // HIGH
+      // cnt - return value of current row
+      PORTD = PORTD & B01111111;
+      flag = false;
+      sensor_ROM++;
+      if(sensor_ROM == 6){
+        countingCycleFinished = true;
+      } else if(sensor_ROM == 7){
+        sensor_ROM = 0;
+      }
+    }
+  }
+}
+
+void touchCounter_ROM(){  
+  // Reads input on pin 2 (PD2)
+  if (PIND & B00000010) touchMatrix_ROM[sensor_ROM]++;
+
+  if(countingCycleFinished){
+    for (i = 0; i <= 6; i++){
+      if(touchMatrix_ROM[i] >= benchmark_ROM[i]){
+        if(i < 3){
+          row_ROM[i] = 1;
+        } else{
+          column_ROM[i] = 1;
+        }
+      } else{
+        if(i < 3){
+          row_ROM[i] = 0;
+        } else{
+          column_ROM[i] = 0;
+        }
+      }
+    }
+
+    for (i = 0; i <= 2; i++){
+      for (k = 0; k <= 3; k++){
+        if((row_ROM[i] == 1) && (column_ROM[k] == 1)){
+          touch[i][k] = 1;
+        } else{
+          touch[i][k] = 0;
+        }
+      }
+    }
+
+    memset(touchMatrix_ROM, 0, sizeof(touchMatrix_ROM)); // Clears touch matrix
+  }
+}
+
+
+///////////////////////////////////////////////////////////
+//////////////////PASSWORD LOGIC///////////////////////////
+///////////////////////////////////////////////////////////
 
 int determineWhatButtonWasPressed(){
   int button = -1;
@@ -216,4 +315,23 @@ int determineWhatButtonWasPressed(){
     button = 11;
   }
   return button;
+}
+
+// Enables user to change password
+void newCode(int buf[], int size) {
+  int key;
+  int j=0;
+
+  for (int g = 0; g < size; g++) {
+    buf[g] = -1;
+  }
+
+  while (j < size){
+    printf("\nNEW PASSWORD:  ");
+    scanf(" %d", &key);
+    if ((key >= 0) && (key <= 9)){    // If input is between 0-9
+      buf[j] = key;
+      j++;
+    }
+  }
 }
